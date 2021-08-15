f2ba63da127d2691d72e2bb5e41dc74aec054dcd
Madeeha Moazzam <madeehamoazzam2001@gmail.com>
2021-08-15 11:34:37 +0530

    first commit

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ console.log(document.title);
 document.title="learning DOM!!";
 console.log(document.title);
 
+//getElementById
+
 const heading = document.getElementById("Helloaccess");
 console.log(heading);
+
+//querySelectorAll
 
 const head = document.querySelectorAll("p")
 console.log(head);
 
+//getElementByName
+
 const head1 = document.getElementsByTagName("h3")
 console.log(head1);
 
+//getElementsByClassName
+
 const head2 = document.getElementsByClassName("second")
 console.log(head2);
+
+//querySelector
 
 const header = document.querySelector("h1")
 console.log(header)
